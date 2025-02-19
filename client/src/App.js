152819ed927adec097b569e4eda9c7cc3d086cb2
Manoj-0312 <indian945200@gmail.com>
2025-02-19 12:42:59 +0530

    Completed Navbar and react-routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import styled, { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lightTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { useState } from "react";
+import Authentication from "./pages/Authentication";
+import Favourites from "./pages/Favourites";
+import Cart from "./pages/Cart";
+import FoodDetails from "./pages/FoodDetails";
+import FoodListing from "./pages/FoodList";
+import { useSelector } from "react-redux";
 
+const Container = styled.div``;
 
 function App() {
+
+  const [openAuth , setOpenAuth] = useState(false);
+
   return (
-    <div className="App">Hello from Backend</div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar setOpenAuth={setOpenAuth} openAuth={openAuth} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+          {openAuth && (
+            <Authentication setOpenAuth={setOpenAuth} openAuth={openAuth} />
+          )}
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
